d02e906900375a3d803d4a89a8c5e6b9a8f366ea
julian D. Londoño <jlondono@gcg.local>
2022-08-30 16:21:55 -0500

    creacion contraladores y modelos login

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class loginController extends Controller
 {
-    public function validateUser()
+    public function validateUser(Request $request)
     {
         try {
-            $data = '1';
-            return response()->json(['status' => 1, 'message' => $data]);
+            $parametrization = DB::table('users')->get();
+            return response()->json(['status' => 1, 'message' => $parametrization]);
         } catch (\Throwable $th) {
             if ($th->getMessage() !== null) {
                 return response()->json(['status' => 0, 'message' => $th->getMessage() . " en la línea " . $th->getLine()]);
--- app/Models/loginModel.php
+++ app/Models/loginModel.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class loginModel extends Model
 {
     use HasFactory;
+    protected $table = "users";
+    protected $primaryKey = "id";
+    public $incrementing = false;
+    public $timestamps = false;
 }
